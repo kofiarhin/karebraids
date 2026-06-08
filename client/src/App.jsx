d2b75e472b474bd1ec30cf4ca9c575b66c013666
Kofi Arhin <kofiarhin69@gmail.com>
2026-06-08 08:03:38 +0100

    Add scroll restoration manager

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { Layout } from './components/Layout.jsx'
+import { RouteScrollManager } from './components/RouteScrollManager.jsx'
 import { StyleRedirect } from './components/StyleRedirect.jsx'
 import { About } from './pages/About.jsx'
 import { Admin } from './pages/Admin.jsx'
@@ -12,20 +13,24 @@ import { ServiceDetail } from './pages/ServiceDetail.jsx'
 
 const App = () => {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="gallery" element={<Gallery />} />
-        <Route path="services" element={<Services />} />
-        <Route path="services/:slug" element={<ServiceDetail />} />
-        <Route path="styles/:slug" element={<StyleRedirect />} />
-        <Route path="booking" element={<Booking />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="admin" element={<Admin />} />
-        <Route path="*" element={<Navigate replace to="/" />} />
-      </Route>
-    </Routes>
+    <>
+      <RouteScrollManager />
+
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="services" element={<Services />} />
+          <Route path="services/:slug" element={<ServiceDetail />} />
+          <Route path="styles/:slug" element={<StyleRedirect />} />
+          <Route path="booking" element={<Booking />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="admin" element={<Admin />} />
+          <Route path="*" element={<Navigate replace to="/" />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
