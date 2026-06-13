e831991709aade45490e2ec704748a9a5cc77d3d
Kofi Arhin <kofiarhin69@gmail.com>
2026-06-13 23:14:52 +0100

    Load light theme solid background overrides

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './light-theme-solid.css'
 import App from './App.jsx'
 import { AppProviders } from './redux/providers.jsx'
 
